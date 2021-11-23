8168de775fd8abbdce34228627a6ec926a2b68c6
Rumit Amit Patel <rapatel01@email.wm.edu>
2021-11-22 22:50:20 -0500

    Implemented LosingActivity

---
+++ app/src/main/java/edu/wm/cs/cs301/RumitPatel/gui/LosingActivity.java
@@ -0,0 +1,69 @@
+package edu.wm.cs.cs301.RumitPatel.gui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import edu.wm.cs.cs301.RumitPatel.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LosingActivity extends AppCompatActivity {
+    private String energyConsumption;
+    private String pathLength;
+    private String shortestPath;
+    private TextView Consumption;
+    private TextView Length;
+    private TextView Path;
+
+    /**
+     * UI for Losing activity
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_losing);
+
+        Intent getIntent = getIntent();
+        energyConsumption = getIntent.getStringExtra("Energy Consumption");
+        pathLength = getIntent.getStringExtra("Path Length");
+        shortestPath = getIntent.getStringExtra("Distance To Exit From Start");
+
+
+
+        //CHANGE FOR PROJECT 7
+        ImageView losingImage = findViewById(R.id.losingImage);
+        if (4500 >= 3500) {
+            losingImage.setBackgroundResource(R.drawable.battery);
+        }
+        else {
+            losingImage.setBackgroundResource(R.drawable.broken);
+        }
+
+        Consumption = findViewById(R.id.energyConsumption);
+        Path = findViewById(R.id.shortestPath);
+        Length = findViewById(R.id.pathLegnth);
+        Consumption.setText("Energy Consumption: "+energyConsumption);
+        Path.setText("Shortest Path: " + shortestPath);
+        Length.setText("Your Path: " + pathLength);
+
+
+
+
+
+    }
+
+
+        /**
+         * take user to AMazeActivity if back button is pressed
+         */
+        @Override
+        public void onBackPressed () {
+            Intent intent = new Intent(this, AMazeActivity.class);
+
+            startActivity(intent);
+            finish();
+        }
+    }
